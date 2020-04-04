244f6a48d568d340ede3da37da53340254a1025d
root <naebzadeh.milad@gmail.com>
2020-04-04 06:48:17 +0430

    first commit

--- backup.py
+++ backup.py
@@ -1,12 +1,12 @@
 from conf import *
 import os,logging,colorlog
 def backup():
-  #  try:
-      #  os.system('mkdir -p $HOME/.kube && cp -i {0} $HOME/.kube/config && chown $(id -u):$(id -g) $HOME/.kube/config '.format(path))
-     #   os.system('export KUBECONFIG=$HOME/.kube/config ')
-   # except:
-    #    logging.error('export KUBECONFIG=$HOME/.kube/config')
-       # exit(1)
+    try:
+        os.system('mkdir -p $HOME/.kube && cp -i {0} $HOME/.kube/config && chown $(id -u):$(id -g) $HOME/.kube/config '.format(path))
+        os.system('export KUBECONFIG=$HOME/.kube/config ')
+    except:
+        logging.error('export KUBECONFIG=$HOME/.kube/config')
+        exit(1)
     for i in range(len(namespaces)):
         for j in range(len(resources)):
             if not os.path.exists('{0},{1}/{2}'.format(dir[0],namespaces[i],resources[j])):
