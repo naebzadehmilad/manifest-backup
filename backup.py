e6db7445344a6025adcc1b165772538dfff1ea64
naebzadehmilad <45167308+naebzadehmilad@users.noreply.github.com>
2020-10-09 15:14:16 +0330

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,19 +1,19 @@
-from conf import *
+from conf import conf
 import os,logging,colorlog
 def backup():
     try:
-        os.system('mkdir -p $HOME/.kube && cp -i {0} $HOME/.kube/config && chown $(id -u):$(id -g) $HOME/.kube/config '.format(path))
+        os.system('mkdir -p $HOME/.kube && cp -i {0} $HOME/.kube/config && chown $(id -u):$(id -g) $HOME/.kube/config '.format(conf.path))
         os.system('export KUBECONFIG=$HOME/.kube/config ')
     except:
         logging.error('export KUBECONFIG=$HOME/.kube/config')
         exit(1)
     for i in range(len(namespaces)):
         for j in range(len(resources)):
-            if not os.path.exists('{0}/{1}/{2}'.format(dir[0],namespaces[i],resources[j])):
+            if not os.path.exists('{0}/{1}/{2}'.format(dir[0],conf.namespaces[i],conf.resources[j])):
                 try:
-                    os.makedirs('{0}/{1}/{2}'.format(dir[0],namespaces[i],resources[j]))
+                    os.makedirs('{0}/{1}/{2}'.format(dir[0],conf.namespaces[i],conf.resources[j]))
                 except:
-                    raise OSError("Can't create destination directory ({0}/{1}/{2})!".format(dir[0],namespaces[i],resources[j]))
+                    raise OSError("Can't create destination directory ({0}/{1}/{2})!".format(dir[0],conf.namespaces[i],conf.resources[j]))
 
 
 backup()
